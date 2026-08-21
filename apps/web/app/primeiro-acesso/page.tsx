04ecc7adcf49083fcc56b857fefb5a61e347add8
imperiussp <imperiussp@gmail.com>
2026-08-20 22:51:36 -0300

    Add first administrator bootstrap page

---
+++ apps/web/app/primeiro-acesso/page.tsx
@@ -0,0 +1,5 @@
+import FirstAdminSetup from "../../components/FirstAdminSetup";
+
+export default function FirstAccessPage() {
+  return <main className="loginPage"><div className="loginShell"><a className="brand loginBrand" href="../"><span className="brandMark">I</span><span>IMOBILIARIAS</span></a><FirstAdminSetup /></div></main>;
+}
