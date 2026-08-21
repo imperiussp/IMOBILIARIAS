bd597e66f6524f88e9408831aedc9a7e4001f046
imperiussp <imperiussp@gmail.com>
2026-08-20 22:49:51 -0300

    Add password recovery page

---
+++ apps/web/app/recuperar-senha/page.tsx
@@ -0,0 +1,5 @@
+import PasswordRecoveryForm from "../../components/PasswordRecoveryForm";
+
+export default function PasswordRecoveryPage() {
+  return <main className="loginPage"><div className="loginShell"><a className="brand loginBrand" href="../"><span className="brandMark">I</span><span>IMOBILIARIAS</span></a><PasswordRecoveryForm /></div></main>;
+}
