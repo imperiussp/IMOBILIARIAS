b30b8a1594c7b31e51960179de25d4ea51f20aac
imperiussp <imperiussp@gmail.com>
2026-08-20 22:50:06 -0300

    Add new password page

---
+++ apps/web/app/nova-senha/page.tsx
@@ -0,0 +1,5 @@
+import NewPasswordForm from "../../components/NewPasswordForm";
+
+export default function NewPasswordPage() {
+  return <main className="loginPage"><div className="loginShell"><a className="brand loginBrand" href="../"><span className="brandMark">I</span><span>IMOBILIARIAS</span></a><NewPasswordForm /></div></main>;
+}
