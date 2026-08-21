29fb030ee73bec2b17bf2dbbca7a1bcf5830dcf3
imperiussp <imperiussp@gmail.com>
2026-08-21 14:04:47 -0300

    Add global platform administration page

---
+++ apps/web/app/plataforma/page.tsx
@@ -0,0 +1,24 @@
+import PlatformAdminDashboard from "../../components/PlatformAdminDashboard";
+import PlatformAdminGate from "../../components/PlatformAdminGate";
+
+export default function PlatformAdminPage() {
+  return (
+    <PlatformAdminGate>
+      <main className="adminPage platformAdminPage">
+        <header className="adminTopbar">
+          <div className="container adminNav">
+            <a className="brand" href="../"><span className="brandMark">L</span><span>LENOY IMÓVEIS</span></a>
+            <div className="adminNavActions"><span>Administração global</span><a className="button secondary small" href="../admin/">Painel de imobiliária</a><a className="button primary small" href="../">Ver plataforma</a></div>
+          </div>
+        </header>
+
+        <div className="container platformAdminShell">
+          <section className="adminContent">
+            <div className="adminHeading"><div><span className="eyebrow">ADMINISTRAÇÃO DA PLATAFORMA</span><h1>Visão global do SaaS</h1><p>Imobiliárias, assinaturas, domínios, usuários e volume de imóveis em um único painel da LENOY.</p></div></div>
+            <PlatformAdminDashboard />
+          </section>
+        </div>
+      </main>
+    </PlatformAdminGate>
+  );
+}
