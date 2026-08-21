0a40fbd9b4917d1e779b8942239a5dfe226ab6a8
imperiussp <imperiussp@gmail.com>
2026-08-21 18:40:41 -0300

    Use official LENOY logo in platform admin

--- apps/web/app/plataforma/page.tsx
+++ apps/web/app/plataforma/page.tsx
@@ -4,13 +4,15 @@ import PlatformBillingOverview from "../../components/PlatformBillingOverview";
 import PlatformPlanManager from "../../components/PlatformPlanManager";
 import PlatformSubscriptionManager from "../../components/PlatformSubscriptionManager";
 
+const lenoyLogo = "https://lenoy.com.br/wp-content/uploads/2026/08/LENOY.jpg";
+
 export default function PlatformAdminPage() {
   return (
     <PlatformAdminGate>
       <main className="adminPage platformAdminPage">
         <header className="adminTopbar">
           <div className="container adminNav">
-            <a className="brand" href="../"><img src="../logo-imobiliarias.svg" alt="IMOBILIARIAS" style={{ width: 190, maxWidth: "38vw", height: "auto", display: "block" }} /></a>
+            <a className="brand" href="../"><img src={lenoyLogo} alt="LENOY" style={{ width: 88, maxWidth: "24vw", height: "auto", display: "block", borderRadius: 8 }} /></a>
             <div className="adminNavActions"><span>Administração global</span><a className="button secondary small" href="../admin/">Painel de imobiliária</a><a className="button primary small" href="../">Ver plataforma</a></div>
           </div>
         </header>
