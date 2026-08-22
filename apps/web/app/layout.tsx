3a1a2fbacbcc91437fbfc43166b27419b259a009
imperiussp <imperiussp@gmail.com>
2026-08-22 19:22:31 -0300

    Aplica acabamento premium na página do imóvel

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -14,6 +14,7 @@ import "./platform-showcase.css";
 import "./platform-plans.css";
 import "./preview-live.css";
 import "./premium-reference.css";
+import "./property-premium.css";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://imoveis.lenoy.com.br";
 const lenoyLogo = "https://lenoy.com.br/wp-content/uploads/2026/08/hh.png";
