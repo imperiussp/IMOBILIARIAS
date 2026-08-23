904cb44fe31f722bdafdacd232485d5c37734b64
imperiussp <imperiussp@gmail.com>
2026-08-23 14:45:45 -0300

    Brand registration with LENOY identity

--- apps/web/app/cadastro/page.tsx
+++ apps/web/app/cadastro/page.tsx
@@ -1,10 +1,12 @@
 import RegisterForm from "../../components/RegisterForm";
 
+const lenoyLogo = "https://lenoy.com.br/wp-content/uploads/2026/08/hh.png";
+
 export default function RegisterPage() {
   return (
     <main className="loginPage">
       <div className="loginShell">
-        <a className="brand loginBrand" href="../"><span className="brandMark">I</span><span>IMOBILIARIAS</span></a>
+        <a className="brand loginBrand accessBrandV4" href="../"><img src={lenoyLogo} alt="LENOY IMOBILIÁRIAS" /><span><strong>LENOY IMOBILIÁRIAS</strong><small>Plataforma para imobiliárias</small></span></a>
         <RegisterForm />
       </div>
     </main>
