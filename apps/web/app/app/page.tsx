9a8d394d0c3a12de0ea99056fe8643ce0985e78e
imperiussp <imperiussp@gmail.com>
2026-08-25 20:34:21 -0300

    Aplica identidade dinâmica no cabeçalho do app

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -1,9 +1,11 @@
 import AdminGate from "../../components/AdminGate";
+import MobileAgencyBranding from "../../components/MobileAgencyBranding";
 import MobileWebApp from "../../components/MobileWebApp";
 
 export default function MobileAppPage() {
   return (
     <AdminGate appMode>
+      <MobileAgencyBranding />
       <MobileWebApp />
     </AdminGate>
   );
