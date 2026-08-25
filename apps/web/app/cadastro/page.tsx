8f9808ec0982f7fa163cb19f11a5aba6f9abd617
imperiussp <imperiussp@gmail.com>
2026-08-24 21:38:30 -0300

    Amplia cadastro de imobiliária mantendo responsividade

--- apps/web/app/cadastro/page.tsx
+++ apps/web/app/cadastro/page.tsx
@@ -5,9 +5,9 @@ const lenoyLogo = "https://lenoy.com.br/wp-content/uploads/2026/08/hh.png";
 
 export default function RegisterPage() {
   return (
-    <main className="loginPage">
+    <main className="loginPage registerPage">
       <FreshRegistrationSession />
-      <div className="loginShell">
+      <div className="loginShell registerShell">
         <a className="brand loginBrand accessBrandV4" href="../"><img src={lenoyLogo} alt="LENOY IMOBILIÁRIAS" /><span><strong>LENOY IMOBILIÁRIAS</strong><small>Plataforma para imobiliárias</small></span></a>
         <RegisterForm />
       </div>
