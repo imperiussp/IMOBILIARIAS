f658647840809ccae3b077ac64fa9f3cc8082bb3
imperiussp <imperiussp@gmail.com>
2026-08-26 02:15:44 -0300

    feat: disponibiliza etiquetas comerciais em Meus imóveis

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -1,10 +1,12 @@
 import AdminGate from "../../components/AdminGate";
+import MobilePropertyLabelDock from "../../components/MobilePropertyLabelDock";
 import MobileWebAppV2 from "../../components/MobileWebAppV2";
 
 export default function MobileAppPage() {
   return (
     <AdminGate appMode>
       <MobileWebAppV2 />
+      <MobilePropertyLabelDock />
     </AdminGate>
   );
 }
