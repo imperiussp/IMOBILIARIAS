e63fc140835a50244d74b30b21f83f0e1b963716
imperiussp <imperiussp@gmail.com>
2026-08-26 19:40:44 -0300

    style: load tenant admin header after legacy styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -44,6 +44,7 @@ import "./admin-dashboard-cards-20260826.css";
 import "./admin-dashboard-hardfix-20260826.css";
 import "./menu-cards-20260826.css";
 import "./release-corrections-20260826.css";
+import "./admin-tenant-header-20260826.css";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://imoveis.lenoy.com.br";
 const lenoyLogo = "https://lenoy.com.br/wp-content/uploads/2026/08/hh.png";
