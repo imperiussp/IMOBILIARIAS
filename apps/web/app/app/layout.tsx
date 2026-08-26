67dd2e6d6f8667527ebfee200269c2b06960f429
imperiussp <imperiussp@gmail.com>
2026-08-26 01:10:06 -0300

    fix(app): remove logo fixa duplicada do layout mobile

--- apps/web/app/app/layout.tsx
+++ apps/web/app/app/layout.tsx
@@ -4,26 +4,6 @@ import MedianOneSignalSync from "../../components/MedianOneSignalSync";
 export default function MobileAppLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <style>{`
-        main > header::before {
-          content: "";
-          display: block;
-          width: 38px;
-          height: 38px;
-          flex: 0 0 38px;
-          background: url("https://lenoy.com.br/wp-content/uploads/2026/08/hh.png") center / contain no-repeat;
-        }
-        main > header > div:first-child {
-          margin-right: auto;
-        }
-        @media (max-width: 390px) {
-          main > header::before {
-            width: 34px;
-            height: 34px;
-            flex-basis: 34px;
-          }
-        }
-      `}</style>
       <MedianOneSignalSync />
       {children}
     </>
