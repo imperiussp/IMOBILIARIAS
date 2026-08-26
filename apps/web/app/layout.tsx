d7c90be34b3515a0ee925eb6ae7eeb09da41250b
imperiussp <imperiussp@gmail.com>
2026-08-26 13:25:25 -0300

    style: carregar lote de correções SaaS por último

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -37,6 +37,7 @@ import "./hero-app-bg-20260825.css";
 import "./refinement-20260826.css";
 import "./refinement-20260826b.css";
 import "./hero-background-final-20260826.css";
+import "./platform-batch-corrections-20260826.css";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://imoveis.lenoy.com.br";
 const lenoyLogo = "https://lenoy.com.br/wp-content/uploads/2026/08/hh.png";
