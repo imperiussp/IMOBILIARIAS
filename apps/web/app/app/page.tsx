01d8a3ca477c7b3e2412e692ef6ac25e81fd50a8
imperiussp <imperiussp@gmail.com>
2026-08-26 22:00:04 -0300

    fix: mount native push handler in mobile app

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -1,10 +1,12 @@
 import AdminGate from "../../components/AdminGate";
+import MedianOneSignalSync from "../../components/MedianOneSignalSync";
 import MobilePropertyLabelDock from "../../components/MobilePropertyLabelDock";
 import MobileWebAppV2 from "../../components/MobileWebAppV2";
 
 export default function MobileAppPage() {
   return (
     <AdminGate appMode>
+      <MedianOneSignalSync />
       <MobileWebAppV2 />
       <MobilePropertyLabelDock />
     </AdminGate>
