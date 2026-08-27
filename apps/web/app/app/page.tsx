37635ea09e9cab6dd1f565a860d5976898518120
imperiussp <imperiussp@gmail.com>
2026-08-26 22:01:27 -0300

    fix: mount notification lead detail in mobile app

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -2,12 +2,14 @@ import AdminGate from "../../components/AdminGate";
 import MedianOneSignalSync from "../../components/MedianOneSignalSync";
 import MobilePropertyLabelDock from "../../components/MobilePropertyLabelDock";
 import MobileWebAppV2 from "../../components/MobileWebAppV2";
+import NotificationLeadDetailMount from "../../components/NotificationLeadDetailMount";
 
 export default function MobileAppPage() {
   return (
     <AdminGate appMode>
       <MedianOneSignalSync />
       <MobileWebAppV2 />
+      <NotificationLeadDetailMount />
       <MobilePropertyLabelDock />
     </AdminGate>
   );
