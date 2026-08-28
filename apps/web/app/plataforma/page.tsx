fc6ca8bbc269ce549eb5be756791c2a180c92056
imperiussp <imperiussp@gmail.com>
2026-08-27 22:49:16 -0300

    refactor: make platform admin commercial-first

--- apps/web/app/plataforma/page.tsx
+++ apps/web/app/plataforma/page.tsx
@@ -1,48 +1,28 @@
-import PlatformAdminDashboard from "../../components/PlatformAdminDashboard";
 import PlatformAdminGate from "../../components/PlatformAdminGate";
 import PlatformBillingOverview from "../../components/PlatformBillingOverview";
-import PlatformDeploymentCheckpoints from "../../components/PlatformDeploymentCheckpoints";
-import PlatformDeploymentReleases from "../../components/PlatformDeploymentReleases";
-import PlatformDocumentTemplateManager from "../../components/PlatformDocumentTemplateManager";
-import PlatformHomologationReadiness from "../../components/PlatformHomologationReadiness";
-import PlatformOperationsDashboard from "../../components/PlatformOperationsDashboard";
+import PlatformCommercialDashboard from "../../components/PlatformCommercialDashboard";
 import PlatformPlanManager from "../../components/PlatformPlanManager";
-import PlatformReleaseControls from "../../components/PlatformReleaseControls";
-import PlatformReleaseHistory from "../../components/PlatformReleaseHistory";
-import PlatformReleaseValidations from "../../components/PlatformReleaseValidations";
 import PlatformSubscriptionManager from "../../components/PlatformSubscriptionManager";
-import PlatformTechnicalHealth from "../../components/PlatformTechnicalHealth";
-import PlatformTenantSecurityAudit from "../../components/PlatformTenantSecurityAudit";
 
 const lenoyLogo = "https://lenoy.com.br/wp-content/uploads/2026/08/hh.png";
 
 export default function PlatformAdminPage() {
   return (
     <PlatformAdminGate>
-      <main className="adminPage platformAdminPage">
+      <main className="adminPage platformAdminPage platformCommercialPage">
         <header className="adminTopbar">
           <div className="container adminNav">
             <a className="brand" href="../"><img src={lenoyLogo} alt="LENOY IMOBILIÁRIAS" style={{ width: 88, maxWidth: "24vw", height: "auto", display: "block" }} /></a>
-            <div className="adminNavActions"><span>Administração global</span><a className="button secondary small" href="../admin/">Painel de imobiliária</a><a className="button primary small" href="../">Ver plataforma</a></div>
+            <div className="adminNavActions"><span>Administração comercial</span><a className="button secondary small" href="./tecnico/">Área técnica</a><a className="button secondary small" href="../admin/">Painel de imobiliária</a><a className="button primary small" href="../">Ver plataforma</a></div>
           </div>
         </header>
 
-        <div className="container platformAdminShell">
+        <div className="container platformAdminShell platformCommercialShell">
           <section className="adminContent">
-            <div className="adminHeading"><div><span className="eyebrow">ADMINISTRAÇÃO DA PLATAFORMA</span><h1>Visão global do SaaS</h1><p>Imobiliárias, assinaturas, cobrança, domínios, usuários, documentos, volume de imóveis e configuração comercial em um único painel da LENOY IMOBILIÁRIAS.</p></div></div>
-            <PlatformReleaseControls />
-            <PlatformHomologationReadiness />
-            <PlatformTechnicalHealth />
-            <PlatformDeploymentCheckpoints />
-            <PlatformDeploymentReleases />
-            <PlatformTenantSecurityAudit />
-            <PlatformReleaseValidations />
-            <PlatformReleaseHistory />
-            <PlatformAdminDashboard />
-            <PlatformOperationsDashboard />
+            <div className="adminHeading commercialHeading"><div><span className="eyebrow">ADMINISTRAÇÃO COMERCIAL</span><h1>Clientes, planos e pagamentos</h1><p>Aqui ficam somente as informações para administrar vendas: quem se cadastrou, quem pagou, qual plano possui, descontos, vencimentos, inadimplência e histórico financeiro.</p></div></div>
+            <PlatformCommercialDashboard />
             <PlatformSubscriptionManager />
             <PlatformPlanManager />
-            <PlatformDocumentTemplateManager />
             <PlatformBillingOverview />
           </section>
         </div>
