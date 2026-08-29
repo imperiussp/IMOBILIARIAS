22f57510ba32bbc55815b1bceee69af7762b36ef
imperiussp <imperiussp@gmail.com>
2026-08-28 21:37:48 -0300

    Aplica fallback de atendimento na página do imóvel

--- apps/web/app/imovel/page.tsx
+++ apps/web/app/imovel/page.tsx
@@ -1,6 +1,12 @@
 import PublicPropertyDetail from "../../components/PublicPropertyDetail";
+import PropertyDetailBrokerFallback from "../../components/PropertyDetailBrokerFallback";
 import "../property-detail-gallery-20260829.css";
 
 export default function OnlinePropertyPage() {
-  return <PublicPropertyDetail />;
+  return (
+    <>
+      <PublicPropertyDetail />
+      <PropertyDetailBrokerFallback />
+    </>
+  );
 }
