3347a3f1a9ec4346ea311d00f1a9c0d8166f56f3
imperiussp <imperiussp@gmail.com>
2026-08-28 21:08:36 -0300

    Aplica galeria nova também na demonstração do site

--- apps/web/app/demonstracao/site/page.tsx
+++ apps/web/app/demonstracao/site/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import DemoBrokerSiteEnhanced from "../../../components/DemoBrokerSiteEnhanced";
 import "../../demo-broker-site-original-20260828.css";
+import "../../property-detail-gallery-20260829.css";
 
 export const metadata: Metadata = {
   title: "Site modelo da imobiliária | LENOY IMOBILIÁRIAS",
