a33519d3ac23a885788342a0d7b06eb7f308ec27
imperiussp <imperiussp@gmail.com>
2026-08-30 22:48:21 -0300

    feat(platform): mount delete user control

--- apps/web/app/plataforma/layout.tsx
+++ apps/web/app/plataforma/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react";
 import PlatformClientVisualFix from "../../components/PlatformClientVisualFix";
+import PlatformClientDeleteControl from "../../components/PlatformClientDeleteControl";
 
 export default function PlatformLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
       <PlatformClientVisualFix />
+      <PlatformClientDeleteControl />
     </>
   );
 }
